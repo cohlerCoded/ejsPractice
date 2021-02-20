0376670033ed4441ab336506698d0d2b3d105b3e
cohlerCoded <jeffcohler@cohlercoded.com>
2021-02-20 15:34:25 -0800

    created template

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.send("HI");
+  res.render("home.ejs");
 });
 app.listen(7000, () => {
   console.log("listening on port 7000");
