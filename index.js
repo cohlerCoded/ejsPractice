aad38e5471a264309e57d2ea4f690e722df284f1
cohlerCoded <jeffcohler@cohlercoded.com>
2021-02-20 14:28:39 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.send("HI");
+});
+app.listen(7000, () => {
+  console.log("listening on port 7000");
+});
