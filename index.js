83637e319a50cfe62e1168aff22084e9c9726325
cohlerCoded <jeffcohler@cohlercoded.com>
2021-02-20 16:45:32 -0800

    passed data to template

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const path = require("path");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
+
 app.get("/", (req, res) => {
   res.render("home");
+});
+app.get("/rand", (req, res) => {
+  const randNum = Math.floor(Math.random() * 10) + 1;
+  res.render("random", { randNum });
 });
 app.listen(7000, () => {
   console.log("listening on port 7000");
