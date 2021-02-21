c097819dad22444939e3dc08a6d966c2449d3bf3
cohlerCoded <jeffcohler@cohlercoded.com>
2021-02-20 17:57:22 -0800

    subreddit template

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.set("views", path.join(__dirname, "/views"));
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  res.render("subreddit", { subreddit });
+});
+
 app.get("/rand", (req, res) => {
   const randNum = Math.floor(Math.random() * 10) + 1;
   res.render("random", { randNum });
