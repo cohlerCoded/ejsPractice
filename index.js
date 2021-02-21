c3f1f0ef9edc68b111f322c3acf293720113ab7e
cohlerCoded <jeffcohler@cohlercoded.com>
2021-02-20 16:13:14 -0800

    added path module to run app from outside dir

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  res.render("home");
 });
 app.listen(7000, () => {
   console.log("listening on port 7000");
