b903bbfe1a022a3bd7a378ff57c52495e95b8138
cohlerCoded <jeffcohler@cohlercoded.com>
2021-02-21 17:41:29 -0800

    embedded data from json into html

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const redditData = require("./data.json");
+console.log(redditData);
 
 app.set("view engine", "ejs");
+
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
@@ -11,16 +14,24 @@ app.get("/", (req, res) => {
 
 app.get("/r/:subreddit", (req, res) => {
   const { subreddit } = req.params;
-  res.render("subreddit", { subreddit });
+  const data = redditData[subreddit];
+  if (data) {
+    res.render("subreddit", { ...data });
+  } else {
+    res.render("notfound", { subreddit });
+  }
 });
+
 app.get("/dogs", (req, res) => {
   const dogs = ["lab", "collie", "poodle", "pitbull", "terrier"];
   res.render("dogs", { dogs });
 });
+
 app.get("/rand", (req, res) => {
   const randNum = Math.floor(Math.random() * 10) + 1;
   res.render("random", { randNum });
 });
+
 app.listen(7000, () => {
   console.log("listening on port 7000");
 });
